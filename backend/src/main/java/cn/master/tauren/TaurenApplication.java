9e979e183cdb0f7a0bdb22bc4376212c3a021762
lijinglin <the2ndindec@gmail.com>
2024-11-29 11:41:16 +0800

    first commit

---
+++ backend/src/main/java/cn/master/tauren/TaurenApplication.java
@@ -0,0 +1,16 @@
+package cn.master.tauren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author the2n
+ */
+@SpringBootApplication
+public class TaurenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaurenApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cn/master/tauren/TaurenApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.master.tauren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaurenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
